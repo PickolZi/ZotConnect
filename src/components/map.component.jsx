5cdbde98eaecbe74fd3a97c0664c93d21225c0d1
TheFalseGod <tranjohnny311@gmail.com>
2024-05-25 14:36:18 -0700

    added bounds to map, set a minimum zoom so uers can not zoom out too far

--- src/components/map.component.jsx
+++ src/components/map.component.jsx
@@ -1,45 +1,40 @@
 
-import { MapContainer, TileLayer, useMap, Marker, Popup,  } from 'react-leaflet';
-
+import { MapContainer, TileLayer, useMap, Marker, Popup,  Rectangle} from 'react-leaflet';
+// import {EditControl} from "react-leaflet-draw"
 const Map = ({events}) => {
-    console.log(events)
-
+    //console.log(events)
+    
     const startingPosition = [33.6424, -117.8417] 
-    const markers = [
-        {
-            location: [33.6424, -117.8437],
-            popUp: [
-                <h1>Name:</h1>,
-                <h1>Location:</h1>,
-                <h1>Event:</h1>
-            ]
-        },
-        {
-            location: [33.6444, -117.8417],
-            popUp: [
-                <h1>Name:</h1>,
-                <h1>Location:</h1>,
-                <h1>Event:</h1>
-            ]
-        }
-    ];
+    const bounds = [
+        [33.6588, -117.8210],
+        [33.6300, -117.8597],
+      ]
+
+    const redColor = { color: 'red' }
     
     return (
         <div className="h-[100%] flex flex-grow bg-pink-300">
             <MapContainer className='h-[100%] w-[100%]' 
             center={startingPosition} 
-            zoom={15} 
+            zoom={15}
+            maxBounds = {bounds}
+            
             scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    minZoom = {15}
+                    
                 />
+                
                 {events.map((event) => (
                     <Marker position={event.geopoint}> 
                         {/* <Popup>{marker.popUp}</Popup> */}
                     </Marker>
                 ))}
-                
+            mapContainer.setMaxZoom(15);
+
+            {/* <Rectangle bounds={bounds} pathOptions={redColor} />         */}
             </MapContainer>
         </div>
     )
