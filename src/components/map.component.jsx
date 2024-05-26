31fd8230fcbf0865f4dd3023c012254f2e58f6ac
PickolZi <pickol876@gmail.com>
2024-05-26 01:25:37 -0700

    Added code to map for jason/johnson regarding panning to specific latlng on map

--- src/components/map.component.jsx
+++ src/components/map.component.jsx
@@ -1,29 +1,29 @@
+import { useState } from 'react';
 
-import { MapContainer, TileLayer, useMap, Marker, Popup,  } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 
 const Map = ({events}) => {
-
+    const [map, setMap] = useState(null);
     const startingPosition = [33.6424, -117.8417] 
     const bounds = [
         [33.6588, -117.8210],
         [33.6300, -117.8597],
-      ]
+    ]
 
-    const redColor = { color: 'red' }
-    
     return (
         <div className="h-[100%] flex flex-grow bg-pink-300">
             <MapContainer className='h-[100%] w-[100%]' 
-            center={startingPosition} 
-            zoom={15}
-            maxBounds = {bounds}
-            
-            scrollWheelZoom={true}>
+                center={startingPosition} 
+                zoom={15}
+                maxBounds={bounds}
+                scrollWheelZoom={true}
+                onClick={handleClick}
+                ref={setMap}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     minZoom = {15}
-                    
                 />
                 
                 {events.map((event) => (
@@ -34,10 +34,15 @@ const Map = ({events}) => {
                         {/* <Popup>{marker.popUp}</Popup> */}
                     </Marker>
                 ))}
-            mapContainer.setMaxZoom(15);
 
-            {/* <Rectangle bounds={bounds} pathOptions={redColor} />         */}
             </MapContainer>
+            
+            <button 
+                className='fixed right-4 top-4 z-[999] bg-black text-white p-2'
+                onClick={() => map.setView([33.6424, -117.8417], map.getZoom()+10)}
+            >
+                Click
+            </button>
         </div>
     )
 }
